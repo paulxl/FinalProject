558d8ca63c08762cf29dd48513c05cac43d9d7b0
javasmuse <candacelewis@mac.com>
2019-10-17 13:12:28 -0600

    minimum database data

--- DB/goorbital.sql
+++ DB/goorbital.sql
@@ -63,9 +63,8 @@ CREATE TABLE IF NOT EXISTS `vehicle` (
   `name` VARCHAR(45) NULL,
   `type` VARCHAR(45) NULL,
   `description` VARCHAR(100) NULL,
-  `altitude` VARCHAR(100) NULL,
+  `range` VARCHAR(100) NULL,
   `capacity` INT NULL,
-  `height` INT NULL,
   `photo_url` VARCHAR(750) NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_vehicle_provider1_idx` (`provider_id` ASC),
@@ -105,7 +104,6 @@ CREATE TABLE IF NOT EXISTS `trip` (
   `destination` VARCHAR(100) NULL,
   `cost` INT NULL,
   `length_trip` INT NULL,
-  `capacity` INT NULL,
   `trip_date` DATETIME NULL,
   `photo_url` VARCHAR(1000) NULL,
   PRIMARY KEY (`id`),
@@ -196,8 +194,20 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `goorbitaldb`;
-INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (1, 'jgleen', '123xyz', 'jglen@buzz.com', NULL, NULL);
-INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (2, 'blightyear', '123xyz', 'blightyear@buzz.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (1, 'spacex', '123xyz', 'buzz@spacex.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (2, 'unitedla', '123xyz', 'buzz@unitedla.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (3, 'blueorigin', '123xyz', 'buzz@blueorigin.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (4, 'virgingalactic', '123xyz', 'buzz@virgalactic.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (5, 'northgrum', '123xyz', 'buzz@northgrum.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (6, 'nasa', '123xyz', 'buzz@nasa.gov', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (7, 'bigelow', '123xyz', 'buzz@bigelow.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (8, 'jglen', '123xyz', 'jglen@gmail.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (9, 'gbluford', '123xyz', 'gbluford@gmail.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (10, 'blightyear', '123xyz', 'blightyear@gmail.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (11, 'sride', '123xyz', 'sride@gmail.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (12, 'jwilliams', '123xyz', 'jwilliams@gmail.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (13, 'boeing', '123xyz', 'boeing@gmail.com', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `role`) VALUES (14, 'sierranc', '123xyz', 'sierranc@gmail.com', NULL, NULL);
 
 COMMIT;
 
@@ -207,7 +217,15 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `goorbitaldb`;
-INSERT INTO `provider` (`id`, `user_id`, `name`, `logo_url`, `web_url`) VALUES (DEFAULT, 1, NULL, NULL, NULL);
+INSERT INTO `provider` (`id`, `user_id`, `name`, `logo_url`, `web_url`) VALUES (1, 1, 'Space X', NULL, 'https://www.spacex.com/');
+INSERT INTO `provider` (`id`, `user_id`, `name`, `logo_url`, `web_url`) VALUES (2, 2, 'United Launch Alliance', NULL, 'https://www.ulalaunch.com/');
+INSERT INTO `provider` (`id`, `user_id`, `name`, `logo_url`, `web_url`) VALUES (3, 3, 'Blue Origin', NULL, 'https://www.blueorigin.com/');
+INSERT INTO `provider` (`id`, `user_id`, `name`, `logo_url`, `web_url`) VALUES (4, 4, 'Virgin Galactic', NULL, 'https://www.virgingalactic.com');
+INSERT INTO `provider` (`id`, `user_id`, `name`, `logo_url`, `web_url`) VALUES (5, 5, 'Northrup Grumman ', NULL, 'https://www.northropgrumman.com/MediaResources/MediaKits/Space/Home.aspx?utm_source=DigitalAd&utm_medium=Redirect&utm_campaign=SpaceOrig+Redirect');
+INSERT INTO `provider` (`id`, `user_id`, `name`, `logo_url`, `web_url`) VALUES (6, 6, 'NASA', NULL, 'www.nasa.gov');
+INSERT INTO `provider` (`id`, `user_id`, `name`, `logo_url`, `web_url`) VALUES (7, 7, 'Bigelow Aerospace ', NULL, 'https://bigelowaerospace.com/');
+INSERT INTO `provider` (`id`, `user_id`, `name`, `logo_url`, `web_url`) VALUES (8, 13, 'Boeing', NULL, 'https://www.boeing.com/space/');
+INSERT INTO `provider` (`id`, `user_id`, `name`, `logo_url`, `web_url`) VALUES (9, 14, 'Sierra Nevada Corporation', NULL, 'https://www.sncorp.com/');
 
 COMMIT;
 
@@ -217,7 +235,35 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `goorbitaldb`;
-INSERT INTO `vehicle` (`id`, `provider_id`, `name`, `type`, `description`, `altitude`, `capacity`, `height`, `photo_url`) VALUES (1, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `vehicle` (`id`, `provider_id`, `name`, `type`, `description`, `range`, `capacity`, `photo_url`) VALUES (1, 3, 'Bionic Space Vehicle', 'passenger', 'passenger and crew transports within low earth orbit', 'LEO', 8, NULL);
+INSERT INTO `vehicle` (`id`, `provider_id`, `name`, `type`, `description`, `range`, `capacity`, `photo_url`) VALUES (2, 8, 'CST-100', 'passenger', 'passenger and crew transports within low earth orbit', 'LEO', 7, NULL);
+INSERT INTO `vehicle` (`id`, `provider_id`, `name`, `type`, `description`, `range`, `capacity`, `photo_url`) VALUES (3, 9, 'Dream Chaser', 'passenger', 'passenger and crew transports within low earth orbit', 'LEO', 7, NULL);
+INSERT INTO `vehicle` (`id`, `provider_id`, `name`, `type`, `description`, `range`, `capacity`, `photo_url`) VALUES (4, 8, 'Dragon 2', 'passenger', 'passenger and crew transports within low earth orbit', 'LEO', 7, NULL);
+INSERT INTO `vehicle` (`id`, `provider_id`, `name`, `type`, `description`, `range`, `capacity`, `photo_url`) VALUES (5, 8, 'Starship', 'passenger', 'passenger and crew transports to Mars', 'Mars', 100, NULL);
+INSERT INTO `vehicle` (`id`, `provider_id`, `name`, `type`, `description`, `range`, `capacity`, `photo_url`) VALUES (6, 1, 'Dragon', 'passenger', 'passenger and crew transports within low earth orbit', 'Suborbital', 6, NULL);
+INSERT INTO `vehicle` (`id`, `provider_id`, `name`, `type`, `description`, `range`, `capacity`, `photo_url`) VALUES (7, 3, 'New Shepard', 'passenger', 'passenger and crew transports within low earth orbit', 'Suborbital', 6, NULL);
+INSERT INTO `vehicle` (`id`, `provider_id`, `name`, `type`, `description`, `range`, `capacity`, `photo_url`) VALUES (8, 5, 'Omega ', 'passenger', 'passenger and crew transports within low earth orbit', 'Suborbital', 15, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `launchport`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `goorbitaldb`;
+INSERT INTO `launchport` (`id`, `name`, `latitude`, `longitude`) VALUES (1, 'Spaceport America', 32.99, 106.97);
+INSERT INTO `launchport` (`id`, `name`, `latitude`, `longitude`) VALUES (2, 'Mojave Air and Space Port', 35.06, 118.15);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `trip`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `goorbitaldb`;
+INSERT INTO `trip` (`id`, `provider_id`, `vehicle_id`, `launchport_id`, `title`, `destination`, `cost`, `length_trip`, `trip_date`, `photo_url`) VALUES (1, 3, 1, 1, 'Lower Earth Experience', 'Earth return', 15000, 1, NULL, NULL);
 
 COMMIT;
 
@@ -227,7 +273,21 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `goorbitaldb`;
-INSERT INTO `traveler` (`id`, `user_id`, `first_name`, `last_name`, `photo_url`) VALUES (1, 1, 'John', 'Gleen', NULL);
+INSERT INTO `traveler` (`id`, `user_id`, `first_name`, `last_name`, `photo_url`) VALUES (1, 8, 'John', 'Glen', NULL);
+INSERT INTO `traveler` (`id`, `user_id`, `first_name`, `last_name`, `photo_url`) VALUES (2, 9, 'Guy', 'Bluford', NULL);
+INSERT INTO `traveler` (`id`, `user_id`, `first_name`, `last_name`, `photo_url`) VALUES (3, 10, 'Buzz', 'Lightyear', NULL);
+INSERT INTO `traveler` (`id`, `user_id`, `first_name`, `last_name`, `photo_url`) VALUES (4, 11, 'Sally', 'Ride', NULL);
+INSERT INTO `traveler` (`id`, `user_id`, `first_name`, `last_name`, `photo_url`) VALUES (5, 12, 'Jeffery', 'Williams', NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `traveler_trip`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `goorbitaldb`;
+INSERT INTO `traveler_trip` (`id`, `trip_id`, `traveler_id`, `date_signed`, `date_completed`, `rating`, `review`, `trip_note`) VALUES (1, 1, 2, NULL, NULL, 5, 'the view was amazing', 'take favorite sunglasses');
 
 COMMIT;
 
