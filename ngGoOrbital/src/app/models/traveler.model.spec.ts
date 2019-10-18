ef7e4486f4a89dd7c79e8377b35151b69188525c
Gabriel Antonia <Gabriel.Antonia@Outlook.com>
2019-10-18 09:26:00 -0600

    Added models and services

---
+++ ngGoOrbital/src/app/models/traveler.model.spec.ts
@@ -0,0 +1,7 @@
+import { Traveler.Model } from './traveler.model';
+
+describe('Traveler.Model', () => {
+  it('should create an instance', () => {
+    expect(new Traveler.Model()).toBeTruthy();
+  });
+});
