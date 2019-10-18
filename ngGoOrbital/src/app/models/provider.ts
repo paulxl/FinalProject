702fecd760c7c5dde31e7ea697befd4d717e9b95
Gabriel Antonia <Gabriel.Antonia@Outlook.com>
2019-10-18 16:52:15 -0600

    added deserialization to models

--- ngGoOrbital/src/app/models/provider.ts
+++ ngGoOrbital/src/app/models/provider.ts
@@ -1,9 +1,15 @@
+import { Deserializable } from './../interfaces/deserializable';
 import { User } from './user';
 
-export class Provider {
+export class Provider implements Deserializable {
   id: number;
   user: User;
   name: string;
   logoURL: string;
   webURL: string;
+  deserialize(input: any): this {
+    Object.assign(this, input);
+    this.user = new User().deserialize(input.user)
+    return this;
+  }
 }
