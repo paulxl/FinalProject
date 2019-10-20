f987fab77ed5891728452d36497615cff23ee4e7
asdf <carl.k.lawson@gmail.com>
2019-10-20 12:38:17 -0600

    minor typo issues

--- ngGoOrbital/src/app/app.module.ts
+++ ngGoOrbital/src/app/app.module.ts
@@ -1,33 +1,32 @@
-import { MessageService } from './services/message.service';
-import { CompaniesService } from './services/companies.service';
-import { TripService } from './services/trip.service';
-import { ProviderService } from './services/provider.service';
-import { UserService } from './services/user.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MessageService } from "./services/message.service";
+import { CompaniesService } from "./services/companies.service";
+import { TripService } from "./services/trip.service";
+// import { ProviderService } from './services/provider.service';
+import { UserService } from "./services/user.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 
-import { TravelerService } from './services/traveler.service';
-import { UserListComponent } from './components/user-list/user-list.component';
-import { TravelerListComponent } from './components/traveler-list/traveler-list.component';
-import { TripListComponent } from './components/trip-list/trip-list.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MainComponent } from './components/main/main.component';
-import { RegisterComponent } from './components/register/register.component';
+import { TravelerService } from "./services/traveler.service";
+import { UserListComponent } from "./components/user-list/user-list.component";
+import { TravelerListComponent } from "./components/traveler-list/traveler-list.component";
+import { TripListComponent } from "./components/trip-list/trip-list.component";
+import { HttpClientModule } from "@angular/common/http";
+import { MainComponent } from "./components/main/main.component";
+import { RegisterComponent } from "./components/register/register.component";
 
-import { SearchComponent } from './components/search/search.component';
-import { DetailsComponent } from './components/details/details.component';
-import { CompaniesComponent } from './components/companies/companies.component';
+import { SearchComponent } from "./components/search/search.component";
+import { DetailsComponent } from "./components/details/details.component";
+import { CompaniesComponent } from "./components/companies/companies.component";
 
-import { TravelerComponent } from './components/traveler/traveler.component';
+import { TravelerComponent } from "./components/traveler/traveler.component";
 
-import { LoginComponent } from './components/login/login.component';
-import { LogoutComponent } from './components/logout/logout.component';
-import { FormsModule } from '@angular/forms';
-
+import { LoginComponent } from "./components/login/login.component";
+import { LogoutComponent } from "./components/logout/logout.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -44,21 +43,10 @@ import { FormsModule } from '@angular/forms';
     TravelerComponent,
     LoginComponent,
     LogoutComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
 
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-  ],
-
-  providers:
-  [TravelerService,
-    UserService,
-    CompaniesService,
-    TripService
-  ],
+  providers: [TravelerService, UserService, CompaniesService, TripService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- ngGoOrbital/src/app/models/companies.ts
+++ ngGoOrbital/src/app/models/companies.ts
@@ -1,31 +1,23 @@
 export class Companies {
+  id: number;
+  userId: number;
+  name: string;
+  logoURL: string;
+  webURL: string;
 
-    // private id : number;
-    // private userId : number;
-    // private name : string;
-    // private logoURL : string;
-    // private webURL : string;
-
-    constructor( 
-        private id?: number,
-        private userId?: number,
-        private name?: string,
-        private logoURL?: string,
-        private webURL?: string){}
-    //  : {
-    //     id?:number;
-    //     userId?:number;
-    //     name?:string;
-    //     logoURL?:string;
-    //     webURL?:string
-    // }={})
-    
-    // {
-    // this.id = id;
-    // this.userId = userId;
-    // this.name = name;
-    // this.logoURL = logoURL;
-    // this.webURL = webURL;
-
-    // }
+  constructor(
+    id?: number,
+    userId?: number,
+    name?: string,
+    logoURL?: string,
+    webURL?: string
+  ) {
+    {
+      this.id = id;
+      this.userId = userId;
+      this.name = name;
+      this.logoURL = logoURL;
+      this.webURL = webURL;
+    }
+  }
 }
