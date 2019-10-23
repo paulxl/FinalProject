84763f6b21a0c0877df89aa9de20b012cb4708e2
asdf <carl.k.lawson@gmail.com>
2019-10-22 19:48:01 -0600

    app module ts imports fixed

--- ngGoOrbital/src/app/app.module.ts
+++ ngGoOrbital/src/app/app.module.ts
@@ -19,22 +19,21 @@ import { TripListComponent } from "./components/trip-list/trip-list.component";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { MainComponent } from "./components/main/main.component";
 import { RegisterComponent } from "./components/register/register.component";
-import { SearchComponent } from './components/search/search.component';
-import { DetailsComponent } from './components/details/details.component';
-import { CompaniesComponent } from './components/companies/companies.component';
+import { SearchComponent } from "./components/search/search.component";
+import { DetailsComponent } from "./components/details/details.component";
+import { CompaniesComponent } from "./components/companies/companies.component";
 
-import { TravelerComponent } from './components/traveler/traveler.component';
+import { TravelerComponent } from "./components/traveler/traveler.component";
 
-
-import { LoginComponent } from './components/login/login.component';
-import { LogoutComponent } from './components/logout/logout.component';
-import { FormsModule } from '@angular/forms';
-import { HttpInterceptorService } from './services/http-interceptor.service';
-import { MenuComponent } from './components/menu/menu.component';
-import { FooterComponent } from './template/footer/footer.component';
-import { ShowAllComponent } from './components/show-all/show-all.component';
-import { AccordianComponent } from './components/show-all/accordian/accordian.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from "./components/login/login.component";
+import { LogoutComponent } from "./components/logout/logout.component";
+import { FormsModule } from "@angular/forms";
+import { HttpInterceptorService } from "./services/http-interceptor.service";
+import { MenuComponent } from "./components/menu/menu.component";
+import { FooterComponent } from "./template/footer/footer.component";
+import { ShowAllComponent } from "./components/show-all/show-all.component";
+import { AccordianComponent } from "./components/show-all/accordian/accordian.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -55,11 +54,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     UserListComponent,
     AccordianComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, NgbModule, BrowserAnimationsModule,
-  LoginComponent,
-  LogoutComponent,
-  MenuComponent,
-  FooterComponent
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+    BrowserAnimationsModule
   ],
 
   providers: [
@@ -76,4 +77,4 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
